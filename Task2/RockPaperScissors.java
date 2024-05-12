4175c50489d6726f91b7fc993c3440439b736e4f
dev-vicky-29 <vickydeshmukh2509@gmail.com>
2024-05-12 22:18:23 +0530

    Update RockPaperScissors.java

--- Task2/RockPaperScissors.java
+++ Task2/RockPaperScissors.java
@@ -2,7 +2,7 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class RockPaperScissors {
-
+ 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         Random random = new Random();
